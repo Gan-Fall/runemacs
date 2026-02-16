4b2471cfe59183bf7494e88cf032a1c53947f587
Gan-Fall <98006378+Gan-Fall@users.noreply.github.com>
2026-02-16 14:54:10 -0500

    Add use-package package manager
    Note require package and require use-package are there to ensure our
    package managers are loaded. Melpa-Stable is not in the repo list for
    now.
    
    use-package-always-ensure t will make every package have an implicit
    ensure by default. Meaning the packages have to be installed and
    available before being loaded by a hook or whatever trigger you've set
    for them. For more info see the use-package user manual's :ensure
    section.

--- init.el
+++ init.el
@@ -34,3 +34,21 @@
 ;; Same but for fill column
 (setq display-fill-column-indicator-column 80)
 (add-hook 'prog-mode-hook 'display-fill-column-indicator-mode)
+
+;; Initialize package sources
+(require 'package)
+
+(setq package-archives '(("melpa" . "https://melpa.org/packages/")
+                         ("org" . "https://orgmode.org/elpa/")
+                         ("elpa" . "https://elpa.gnu.org/packages/")))
+
+(package-initialize)
+(unless package-archive-contents
+ (package-refresh-contents))
+
+;; Initialize use-package on non-Linux platforms
+(unless (package-installed-p 'use-package)
+   (package-install 'use-package))
+
+(require 'use-package)
+(setq use-package-always-ensure t)
