06f56638ca5ff92f19c03f0d4af276dcb847a599
Gan-Fall <98006378+Gan-Fall@users.noreply.github.com>
2026-02-16 14:10:47 -0500

    Add some optional tweaks
    Comment and uncomment them at will. Also formatted the code a lil bit.

--- init.el
+++ init.el
@@ -1,17 +1,22 @@
 (setq inhibit-startup-message t)
-
 (scroll-bar-mode -1)        ; Disable visible scrollbar
 (tool-bar-mode -1)          ; Disable the toolbar
 (tooltip-mode -1)           ; Disable tooltips
 (set-fringe-mode 10)        ; Give some breathing room
-
 (menu-bar-mode -1)            ; Disable the menu bar
 
 (defvar runemacs/default-font-size 160)
 (set-face-attribute 'default nil :font "FiraCode Nerd Font Mono" :height runemacs/default-font-size)
-
-;; Set the fixed pitch face
 (set-face-attribute 'fixed-pitch nil :font "FiraCode Nerd Font Mono" :height runemacs/default-font-size)
-
-;; Set the variable pitch face
 (set-face-attribute 'variable-pitch nil :font "Iosevka Aile" :height 195 :weight 'regular)
+
+;; Make ESC quit prompts
+(global-set-key (kbd "<escape>") 'keyboard-escape-quit)
+
+;; Swap C-g and C-c
+;(define-key key-translation-map (kbd "C-g") (kbd "C-c"))
+;(define-key key-translation-map (kbd "C-c") (kbd "C-g"))
+
+;; Configure the bell
+(setq visible-bell t)
+;;(setq ring-bell-function 'beep)
