8f4a3b0660ff7ae62060bddb9260911af430977b
Gan-Fall <98006378+Gan-Fall@users.noreply.github.com>
2026-05-15 14:49:28 -0400

    Add helpful

---
+++ packages.d/helpful.el
@@ -0,0 +1,7 @@
+(use-package helpful
+  :bind (("C-h f" . helpful-callable)
+	 ("C-h v" . helpful-variable)
+	 ("C-h k" . helpful-key)
+	 ("C-h x" . helpful-command)
+	 ("C-h F" . helpful-function)
+	 ("C-h C-." . helpful-at-point)) )
