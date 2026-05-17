4b8c0bd2bbf8fa08aecffccdeb01516c095cc684
Gan-Fall <98006378+Gan-Fall@users.noreply.github.com>
2026-05-16 22:53:47 -0400

    Add lsp-mode
    So far breadcrumb header has not worked but I'll add a fix if it needs
    one soon.

---
+++ packages.d/lsp-mode.el
@@ -0,0 +1,21 @@
+(defun efs/lsp-mode-setup ()
+  (setq lsp-headerline-breadcrumb-segments '(path-up-to-project file symbols))
+  (lsp-headerline-breadcrumb-mode))
+
+(use-package lsp-mode
+  :init
+  ;; set prefix for lsp-command-keymap (few alternatives - "C-l", "C-c l")
+  (setq lsp-keymap-prefix "C-c l")
+  :hook (;; replace XXX-mode with concrete major-mode(e. g. python-mode)
+         (ruby-mode . lsp-deferred)
+         (ruby-ts-mode . lsp-deferred)
+	 ;; Don't forget to install emmet
+         (javascript-mode . lsp-deferred)
+         (html-mode . lsp-deferred)
+         (html-ts-mode . lsp-deferred)
+         (css-mode . lsp-deferred)
+         (css-ts-mode . lsp-deferred)
+         ;; if you want which-key integration
+         (lsp-mode . efs/lsp-mode-setup)
+         (lsp-mode . lsp-enable-which-key-integration))
+  :commands lsp lsp-deferred)
